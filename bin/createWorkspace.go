d79b38eb3bda26330bc219019c9ece04c00d6c26
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2014-10-18 17:02:21 +0200

    changed to direct clone

--- bin/createWorkspace.go
+++ bin/createWorkspace.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/url"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 const autoenv = `
@@ -22,11 +24,31 @@ func check(msg string, err error) {
 }
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("use with a projectname, eg. github.com/ulrichSchreiner/gl")
+		fmt.Println("use with a git repo, eg. git@github.com:ulrichSchreiner/gl.git")
 		return
 	}
-	pt := os.Args[1]
-	dir, wks := filepath.Split(pt)
+	u, e := url.Parse(os.Args[1])
+	check("parse given url", e)
+
+	if u.Scheme == "" {
+		// ssh git repo
+		u, e = url.Parse("ssh://" + strings.Replace(os.Args[1], ":", "/", 1))
+	}
+	var cmd string
+	if u.User != nil {
+		cmd = u.User.Username()
+	}
+	// if there is an extension (.git), remove it, we don't need it
+	ext := filepath.Ext(u.Path)
+	if ext != "" {
+		u.Path = u.Path[0 : len(u.Path)-len(ext)]
+		// perhaps an anonymous clone via https? use extension as command
+		if cmd == "" {
+			cmd = ext[1:]
+		}
+	}
+
+	dir, wks := filepath.Split(u.Path)
 	wd, err := os.Getwd()
 	check("current workingdir", err)
 
@@ -34,11 +56,14 @@ func main() {
 
 	check("mkdir bin", os.MkdirAll(fmt.Sprintf("%s/bin", wksdir), 0755))
 	check("mkdir pkg", os.MkdirAll(fmt.Sprintf("%s/pkg", wksdir), 0755))
-	check("mkdir src", os.MkdirAll(fmt.Sprintf("%s/src/%s", wksdir, dir), 0755))
+	check("mkdir src", os.MkdirAll(fmt.Sprintf("%s/src/%s/%s", wksdir, u.Host, dir), 0755))
 	check("write .env", ioutil.WriteFile(fmt.Sprintf("%s/.env", wksdir), []byte(autoenv), 0755))
 
-	goget := exec.Command("go", "get", os.Args[1])
-	goget.Dir = fmt.Sprintf("%s/src/%s", wksdir, dir)
+	if cmd == "" {
+		log.Fatal("unknown clone command")
+	}
+	goget := exec.Command(cmd, "clone", os.Args[1])
+	goget.Dir = fmt.Sprintf("%s/src/%s/%s", wksdir, u.Host, dir)
 	goget.Env = []string{fmt.Sprintf("GOPATH=%s", wksdir), fmt.Sprintf("PATH=%s", os.ExpandEnv("$PATH"))}
 	out, err := goget.CombinedOutput()
 	check(string(out), err)
